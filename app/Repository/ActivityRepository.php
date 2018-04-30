790a02a116925034d5dfccbd4895337e88e6c374
vinicius_mooven <vinicius.silva@moovenconsulting.com.br>
2018-04-30 13:33:14 -0300

    update

--- app/Repository/ActivityRepository.php
+++ app/Repository/ActivityRepository.php
@@ -6,16 +6,6 @@ use Illuminate\Support\Facades\DB;
 
 class ActivityRepository{
 
-    public function list(){
-        return DB::table('status')->orderBy('id', 'asc')->get();
-    }
 
-    public static function notCompletedStatusStatic($statusId){
-        return DB::table('status')
-            ->whereRaw("LOWER(name) != 'concluido'")
-            ->where('id', $statusId)
-            ->orderBy('id', 'asc')
-            ->exists();
-    }
 
 }
